599a007e33631f3869752f88879e79846280fbff
RodolfoDev <rodolfosilvaa16@outlook.com>
2021-06-05 00:58:59 -0300

    feature: finalização da api

--- src/config/database.js
+++ src/config/database.js
@@ -3,9 +3,17 @@
 //https://sequelize.org/v5/manual/getting-started.html#installing
 
 module.exports = {
-    production: {
-        url: "postgres://llnlffkmecdbex:310cf49ac31278bd5cdb5553bbd819c165dbd7fef709a58903ca694fae727ab1@ec2-34-232-191-133.compute-1.amazonaws.com:5432/d97al156bgcbvb?sslmode=require",
-        dialect: 'postgres',
+    dialect: 'postgres',
+    host: 'ec2-34-232-191-133.compute-1.amazonaws.com',
+    username: 'llnlffkmecdbex',
+    password: '310cf49ac31278bd5cdb5553bbd819c165dbd7fef709a58903ca694fae727ab1',
+    database: 'd97al156bgcbvb',
+    port: 5432,
+    ssl: true,
+    define: {
+        timestamps: true,// significa q toda tabela vai ter um campo com created_at(Onde armazena a data q o registro foi criado), updated_at(data quer foi atualizada)
+        underscored: true,//coloca um padrao na criação das tabelas 
+        //padrao Snake case ex: nome_databela 
     },
 };
 
