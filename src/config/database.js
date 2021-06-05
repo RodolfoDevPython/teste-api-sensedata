84acaff1bfbd5b6ee624f24ff380fcc429dbc7a3
RodolfoDev <rodolfosilvaa16@outlook.com>
2021-06-05 00:49:56 -0300

    feature: finalização da api

--- src/config/database.js
+++ src/config/database.js
@@ -4,7 +4,7 @@
 
 module.exports = {
     production: {
-        url: process.env.DATABASE_URL,
+        url: "postgres://llnlffkmecdbex:310cf49ac31278bd5cdb5553bbd819c165dbd7fef709a58903ca694fae727ab1@ec2-34-232-191-133.compute-1.amazonaws.com:5432/d97al156bgcbvb",
         dialect: 'postgres',
     },
 };
